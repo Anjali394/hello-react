9d44b4b51fac1cca3fec88d51f8dd2f9e941a205
Anjali Arya <aanjali@athenahealth.com>
2021-12-09 16:05:50 +0530

    added components

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class welcome extends Component {
+    render() {
+        return (
+            <div>
+                Welcome Anjali
+            </div>
+        )
+    }
+}
+
+export default welcome
